083af2af458c75a50767219d52d811e9aa022c7c
David Dias <ddiasrocha@hotmail.com>
2024-08-30 09:42:28 -0300

    FindById implementation

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -48,10 +48,9 @@ public class Department implements Serializable {
 
     @Override
     public String toString() {
-        return "Department{" +
-                "id=" + id +
-                ", name= " + name +
-                "}";
+        return "Department: " +
+                "id = " + id +
+                ", name = " + name;
     }
 
 }
