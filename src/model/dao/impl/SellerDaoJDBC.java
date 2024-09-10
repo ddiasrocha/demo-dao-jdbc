5018fc3fb8103535067fa6d9233eb51e47d634cb
David Dias <ddiasrocha@hotmail.com>
2024-09-10 02:19:48 -0300

    Update Implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -28,9 +28,9 @@ public class SellerDaoJDBC implements SellerDao {
 
             st = conn.prepareStatement(
                     "INSERT INTO seller "
-                      + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-                      + "VALUES "
-                      + "(?, ?, ?, ?, ?)",
+                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                            + "VALUES "
+                            + "(?, ?, ?, ?, ?)",
                     Statement.RETURN_GENERATED_KEYS);
 
             st.setString(1, obj.getName());
@@ -55,7 +55,7 @@ public class SellerDaoJDBC implements SellerDao {
 
         }
         catch(SQLException e){
-           throw new DbException(e.getMessage());
+            throw new DbException(e.getMessage());
         }
         finally {
             DB.closeStatement(st);
@@ -65,6 +65,31 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void update(Seller obj) {
+
+        PreparedStatement st = null;
+        try{
+
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                            + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                            + "WHERE Id = ?");
+
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
+
+            st.executeUpdate();
+
+        }
+        catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
 
     }
 
